c9d5612f41158e699932f3bd12d6bf8d191373e2
joe <zmoonmoozn@gmail.com>
2024-06-24 16:14:52 +0100

    simplified init.el and added mypackages.el

---
+++ .emacs.d/mypackages.el
@@ -0,0 +1,16 @@
+
+;; Add MELPA repository for package management
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+;; Ensure that use-package is installed
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; Enable Evil mode at startup
+(use-package evil
+  :ensure t
+  :config
+  (evil-mode 1))
